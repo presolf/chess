842efc0eafd99e198c7650ab274c1613db71e17e
presolf <presi-mb@web.de>
2023-12-13 14:19:51 +0100

    set screen and gameloop
    screen and gameloop

--- main.py
+++ main.py
@@ -6,6 +6,25 @@ width = 800
 height = 800
 
 window = pygame.display.set_mode([width,height])
+timer = pygame.time.Clock()
+fps = 30
+
+#gameloop
+
+running = True
+while running:
+     timer.tick(fps)
+     window.fill('white')
+
+     for event in pygame.event.get():           #checking if program was quit
+         if event.type == pygame.QUIT:
+             running = False
+
+     pygame.display.flip()
+
+pygame.quit()
+
+
 
 
 
