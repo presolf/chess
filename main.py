5e62b230490db58287395e8539f052da732b9262
presolf <presi-mb@web.de>
2023-12-13 13:40:09 +0100

    with pygame
    new with pygame

--- main.py
+++ main.py
@@ -1,40 +1,11 @@
-import tkinter as tk
+import pygame
 
-width = int(input("What is the width?: "))
-if(width < 100):
-    print("to small, has to be at least 100")
-    exit()
-height = int(input("What is the height?: "))
-if (height < 100):
-    print("to small, has to be at least 100")
-    exit()
+pygame.init()
 
-boardsize = 8
-squaresize = width // boardsize
+width = 800
+height = 800
 
+window = pygame.display.set_mode([width,height])
 
-window = tk.Tk()
-window.title("Schachbrett")
-
-canvas = tk.Canvas(window, width=width, height=height)
-canvas.pack()
-
-def draw_board():
-    colors = ["white","black"]
-    for row in range(boardsize):
-        for columnn in range(boardsize):
-            x = columnn * squaresize
-            y = row * squaresize
-            x2 = x + squaresize
-            y2 = y + squaresize
-            color = colors[(row + columnn) % 2]
-            canvas.create_rectangle(x, y, x2, y2, fill=color)
-
-
-def start():
-    draw_board()
-    window.mainloop()
-
-start()
 
 
